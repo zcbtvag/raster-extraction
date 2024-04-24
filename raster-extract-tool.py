c29fda10f2b0c7d2bc355b5dbf8af13d557dc912
Victor Agrest <victoragrest@cyclops.ai>
2024-04-24 23:57:55 +0000

    shapefile functionality updated to output both full area raster and uniform, smaller rasters

--- raster-extract-tool.py
+++ raster-extract-tool.py
@@ -6,6 +6,7 @@ import fiona
 import rasterio
 import rioxarray as rxr
 import geopandas as gpd
+from shapely.geometry import Polygon 
 from rasterio.mask import mask
 from math import ceil
 from dotenv import load_dotenv
@@ -110,11 +111,61 @@ def shapefile_extractor(res, tile_size, out_path, dest, shapefile, out_crs=None)
     qgs.initQgis()
     Processing.initialize()
 
+    with fiona.open(f'{shapefile}') as shp:
+        if not out_crs:
+            out_crs = int(str(shp.crs).split(':')[1])
+
     shape_gpd = gpd.read_file(f'{shapefile}/{shapefile}.shp')
     updated_shape_gpd = shape_gpd.to_crs('epsg:3857')
     geometries = [geom for geom in list(updated_shape_gpd['geometry'])]
 
-    extent_str = f"{updated_shape_gpd.bounds.iloc[0][0]},{updated_shape_gpd.bounds.iloc[0][2]},{updated_shape_gpd.bounds.iloc[0][1]},{updated_shape_gpd.bounds.iloc[0][3]} [{updated_shape_gpd.crs}]"
+    minx, miny, maxx, maxy = float(updated_shape_gpd.bounds.iloc[0][0]), float(updated_shape_gpd.bounds.iloc[0][2]), float(updated_shape_gpd.bounds.iloc[0][1]), float(updated_shape_gpd.bounds.iloc[0][3])
+
+    tile_size_updated = float(tile_size)
+
+    x_tiles = range(ceil((maxx - minx) / tile_size_updated))
+    y_tiles = range(ceil((maxy - miny) / tile_size_updated))
+
+    for i in x_tiles:
+        for j in y_tiles:
+
+            grid_tile_poly = Polygon([(minx + i * tile_size_updated, miny + j * tile_size_updated),
+                             (minx + (i + 1) * tile_size_updated, miny + j * tile_size_updated),
+                             (minx + (i + 1) * tile_size_updated, miny + (j + 1) * tile_size_updated),
+                             (minx + i * tile_size_updated, miny + (j + 1) * tile_size_updated)])
+            
+            if not geometries[0].intersects(grid_tile_poly):
+                continue
+            else: 
+                grid_tile = QgsRectangle(minx + i * tile_size_updated, 
+                            miny + j * tile_size_updated, 
+                            minx + (i + 1) * tile_size_updated, 
+                            miny + (j + 1) * tile_size_updated)
+
+                extent_str = f"{grid_tile.xMinimum()},{grid_tile.xMaximum()},{grid_tile.yMinimum()},{grid_tile.yMaximum()} [{updated_shape_gpd.crs}]"
+
+                parameters = {
+                    'EXTENT':extent_str,
+                    'EXTENT_BUFFER':0,
+                    'TILE_SIZE':tile_size_updated,
+                    'MAP_UNITS_PER_PIXEL':res,
+                    'MAKE_BACKGROUND_TRANSPARENT':False,
+                    'MAP_THEME':None,
+                    'LAYERS':[f'wms://crs={updated_shape_gpd.crs}&format&type=xyz&url=https://mt1.google.com/vt/lyrs%3Ds%26x%3D%7Bx%7D%26y%3D%7By%7D%26z%3D%7Bz%7D&zmax=22&zmin=0&http-header:referer='],
+                    'OUTPUT':f'{out_path}/georef_raster_{i}_{j}.tif'
+                }
+
+                print('--> Check no. 1.1. In the for loop -- PRE-processing.')
+
+                processing.run('native:rasterize', parameters)
+
+                print('--> Check no. 1.2. In the for loop -- PRE-processing.')
+
+                raster_ex = rxr.open_rasterio(f'{out_path}/georef_raster_{i}_{j}.tif', masked=True)
+                raster_ex.rio.write_crs(out_crs, inplace=True)
+                raster_ex.rio.to_raster(f'{out_path}/georef_raster_{i}_{j}.tif')
+
+    extent_str = f"{updated_shape_gpd.bounds.iloc[0][0]},{updated_shape_gpd.bounds.iloc[0][2]},{updated_shape_gpd.bounds.iloc[0][1]},{updated_shape_gpd.bounds.iloc[0][3]} [{updated_shape_gpd.crs}]"            
 
     parameters = {
                 'EXTENT':extent_str,
@@ -127,16 +178,11 @@ def shapefile_extractor(res, tile_size, out_path, dest, shapefile, out_crs=None)
                 'OUTPUT':f'{out_path}/georef_raster.tif'
         }
 
-    print('--> Check no. 1. Pre-processing.')
+    print('--> Check no. 2.1. PRE-processing.')
 
     processing.run('native:rasterize', parameters)
 
-    print('--> Check no. 2. Post-processing.')
-
-    with fiona.open(f'{shapefile}') as shp:
-
-        if not out_crs:
-            out_crs = int(str(shp.crs).split(':')[1])
+    print('--> Check no. 2.2. POST-processing.')
 
     georef_rst = rxr.open_rasterio(f'{out_path}/georef_raster.tif', masked=True)
     georef_rst.rio.write_crs(out_crs, inplace=True)
@@ -157,13 +203,15 @@ def shapefile_extractor(res, tile_size, out_path, dest, shapefile, out_crs=None)
     with rasterio.open(f'{out_path}/georef_raster.tif', 'w', **out_meta) as dest:
         dest.write(out_image)
 
-    print('--> Check no. 3. Pre-S3 upload.')
-
-    fs.put(f'{out_path}', f's3://{dest}', recursive=True)
-    
-    print('--> Check no. 4. S3 upload complete.')
-
     qgs.exitQgis()
+
+    print('--> Check no. 2.3. PRE--S3 upload.')
+
+    fs.put(f'{out_path}', f's3://{dest}', recursive=True) # Use below if paramvalidationError encountered.
+
+    # fs.put('/root/cyclops/raster-extraction/raster-extraction/shapefile-test', 's3://cyclops-sbx/tmp/raster-extraction', recursive=True)
+    
+    print('--> Check no. 2.4. S3 upload complete.')
 
 def main():
     parser = argparse.ArgumentParser(description='Raster extraction from QGIS baselayer map grid.')
